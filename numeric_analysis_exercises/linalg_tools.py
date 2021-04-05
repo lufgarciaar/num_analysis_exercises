6915c711c1b1b98055ae36ed681742e5a3125bb5
Luis Felipe García Arias <lufgarciaar@unal.edu.co>
2021-04-04 19:27:48 -0500

    cap3_example 7 plot

--- numeric_analysis_exercises/linalg_tools.py
+++ numeric_analysis_exercises/linalg_tools.py
@@ -18,6 +18,12 @@ def lin_sol(augmented_matrix):
     else:
         return 'infinite solutions'
 
+def least_squares(slope, intercept):
+    A = np.zeros((slope.shape[0], 2))
+    A[:, 0] = 1
+    A[:, 1] = -slope[:, 0]
+    return np.linalg.inv(A.T@A)@A.T@intercept
+
 def diagonalize(A):
     eigenvalues_of_A, eigenvectors_of_A = np.linalg.eig(A)    
     diagonal_matrix = np.diagflat(eigenvalues_of_A)
