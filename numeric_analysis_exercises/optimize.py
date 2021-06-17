bfba69a6dd55ef3af4b4dd4cf642764c89b9f59b
Luis Felipe García Arias <lufgarciaar@unal.edu.co>
2021-06-17 01:14:15 -0500

    Chapters 2 and 3 solved (#4)
    * acces to optimize.py file when runs in colab
    
    * comparison of performance in example 2
    
    * Fix chapter name
    
    * File with linter preferences.
    
    * PEP8 refact.

--- numeric_analysis_exercises/optimize.py
+++ numeric_analysis_exercises/optimize.py
@@ -1,21 +1,22 @@
-from numpy import array, linalg, vstack
-from scipy import optimize as opt
-import sympy as sp
 import random
 
+import sympy as sp
+from numpy import array, linalg, vstack
+from scipy import optimize as opt
 from sympy.core.sympify import sympify
 
-def steepest_descent(func, vars, /, start=(0, 0), *,
- err=0.001, max_iter=1000, stop_criteria='sc_diff', step_type='opt', step=0.1, full_output=False):
+
+def steepest_descent(func, sym_vars, /, start=(0, 0), *,
+                     err=0.001, max_iter=1000, stop_criteria='sc_diff', step_type='opt', step=0.1, full_output=False):
     """
     Compute Steepest DescentMethod.
 
     Parameters
     ----------
-        func: sympy.core.mul.Mul 
-            Expression with the function to minimize, returned by sympy. 
+        func: sympy.core.mul.Mul
+            Expression with the function to minimize, returned by sympy.
             x1 and x2 must be used as variables.
-        vars: symbols
+        sym_vars: symbols
             Contains sympy symbols of the used variables in function.
         start: list, optional.
             Start point to search minimum. If not provided, a random point
@@ -48,29 +49,32 @@ def steepest_descent(func, vars, /, start=(0, 0), *,
         points: ndarray, optional.
             Every point evaluated by the algorithm.
     """
-    
+
     if not stop_criteria in ['sc_diff', 'gradient', 'abs_diff']:
-      raise ValueError("stop_criteria is one of 'sc_diff', 'gradient', 'abs_diff'")
+        raise ValueError(
+            "stop_criteria is one of 'sc_diff', 'gradient', 'abs_diff'")
 
     if not step_type in ['opt', 'fix']:
-      raise ValueError("step_type is one of 'opt', 'fix'")
+        raise ValueError("step_type is one of 'opt', 'fix'")
 
-    grad = sp.derive_by_array(func, [*vars])
+    grad = sp.derive_by_array(func, [*sym_vars])
 
-    function = sp.lambdify([*vars], func, modules='numpy')
-    gradient = sp.lambdify([*vars], grad, modules='numpy')
+    function = sp.lambdify([*sym_vars], func, modules='numpy')
+    gradient = sp.lambdify([*sym_vars], grad, modules='numpy')
 
     start = array(start)
 
     opt_boundaries = (0, 10)
 
     if step_type == 'opt':
-        alpha = opt.fminbound(phi, *opt_boundaries, args=(function, gradient, start,))
+        alpha = opt.fminbound(phi, *opt_boundaries,
+                              args=(function, gradient, start,))
     if step_type == 'fix':
         alpha = step
 
     opt_point = start - alpha*array(gradient(*start))
-    stop_flag = term_criterion(function, start, opt_point, criteria=stop_criteria, err=err)
+    stop_flag = term_criterion(
+        function, start, opt_point, criteria=stop_criteria, err=err)
 
     points = start
     points = vstack((points, opt_point))
@@ -79,17 +83,19 @@ def steepest_descent(func, vars, /, start=(0, 0), *,
 
     while not (stop_flag):
         if step_type == 'opt':
-            alpha = opt.fminbound(phi, *opt_boundaries, args=(function, gradient, prev_point,))
+            alpha = opt.fminbound(phi, *opt_boundaries,
+                                  args=(function, gradient, prev_point,))
         if step_type == 'fix':
             alpha = step
         opt_point = prev_point - alpha*array(gradient(*prev_point))
-        stop_flag = term_criterion(function, prev_point, opt_point, criteria=stop_criteria, err=err)
+        stop_flag = term_criterion(
+            function, prev_point, opt_point, criteria=stop_criteria, err=err)
 
-        iter +=1
+        iter += 1
         points = vstack((points, opt_point))
         prev_point = opt_point
 
-        if iter>=max_iter:
+        if iter >= max_iter:
             stop_flag = True
 
     if full_output:
@@ -97,14 +103,16 @@ def steepest_descent(func, vars, /, start=(0, 0), *,
     else:
         return opt_point
 
+
 def phi(alpha, function, gradient, point):
     eval_gradient = array(gradient(*point))
     arg = point-alpha*eval_gradient
     eval_phi = array(function(*arg))
     return eval_phi
 
-def term_criterion(function, prev_point, new_point,*, criteria='sc_diff', err=0.001):
-    
+
+def term_criterion(function, prev_point, new_point, *, criteria='sc_diff', err=0.001):
+
     if criteria == 'sc_diff':
         tol = scaled_difference(function, prev_point, new_point)
 
@@ -118,19 +126,24 @@ def term_criterion(function, prev_point, new_point,*, criteria='sc_diff', err=0.
         criteria_flag = True
     else:
         criteria_flag = False
-        
+
     return criteria_flag
 
+
 def scaled_difference(func, prev_point, new_point):
-    iter_difference = linalg.norm(array(func(*new_point))-array(func(*prev_point)))
-    scl_diff = iter_difference/max(1, linalg.norm(array(func(*prev_point)))) 
+    iter_difference = linalg.norm(
+        array(func(*new_point))-array(func(*prev_point)))
+    scl_diff = iter_difference/max(1, linalg.norm(array(func(*prev_point))))
     return scl_diff
+
 
 def grad_criteria(grad, point):
     eval_grad = array(grad(*point))
     grad_norm = linalg.norm(eval_grad)
     return grad_norm
 
+
 def absolute_difference(func, prev_point, new_point):
-    iter_difference = linalg.norm(array(func(*new_point))-array(func(*prev_point)))
+    iter_difference = linalg.norm(
+        array(func(*new_point))-array(func(*prev_point)))
     return iter_difference
